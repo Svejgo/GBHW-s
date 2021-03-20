1d109236da73bdad4849da66dfb91379b75cbac7
Vasilev V <svejgo@gmail.com>
2021-03-20 18:17:06 +0700

    task1

---
+++ OOP-HW's/Lesson 5/Task1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdint>
+
+template <class T>
+//template <typename U> class Pair<int32_t, U>{};
+
+ class Pair1
+{
+	private:
+		 T m_First;
+		 T m_Second;
+	public:
+		Pair1<T>(T first, T second)
+			:m_First(first),m_Second(second)
+		{}
+		T First() const { return m_First; }
+		T Second() const { return m_Second; }
+
+};
+
+
+int main()
+{
+	Pair1<int32_t> p1(5, 4);
+	Pair1<double> p2(6.4, 9.2);
+	std::cout << "pair: " << p1.First() << ' ' << p1.Second() << '\n';
+	std::cout << "pair: " << p2.First() << ' ' << p2.Second() << '\n';
+	return 0;
+}
+
+
