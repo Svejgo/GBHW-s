df4df45b06d7c20fa1ea45ba915f5039ea973b35
Vasilev V <svejgo@gmail.com>
2021-04-18 12:08:18 +0700

    11

--- STL-C++/HW1/Person.h
+++ STL-C++/HW1/Person.h
@@ -9,7 +9,7 @@ extern struct Person
 	std::string Surname;
 	std::string Firstname;
 	std::string Secondname{};
-
+	
 	friend std::ostream& operator<< (std::ostream& out, const Person& temp);
 	friend bool operator< (const Person& Left, const Person& Right);
 	friend bool operator== (const Person& Left, const Person& Right);
